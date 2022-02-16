d54e6443167012f2ecfc45efecc8a5adc39f3fb4
Carlaav <carla2002.av@gmail.com>
2022-02-16 16:54:07 +0100

    a

---
+++ ejer4.py
@@ -0,0 +1,10 @@
+num1= 1
+num2= 2
+num3= 3
+coeficiente1= 0.2
+coeficiente2= 0.3
+coeficiente3= 0.5
+numerador= num1*coeficiente1+ num2*coeficiente2+ num3*coeficiente3
+denominador= num1+num2+num3
+Media_ponderada= numerador/denominador
+print(Media_ponderada)

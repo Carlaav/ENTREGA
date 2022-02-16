da33a56643f4cf925ddd2978385b749f2053165c
Carlaav <carla2002.av@gmail.com>
2022-02-16 16:40:18 +0100

    a

---
+++ ejer1.py
@@ -0,0 +1,6 @@
+precio_sin_impuestos= 5
+Iva= precio_sin_impuestos*21/100
+if precio_sin_impuestos >=0:
+    print("el precio final es:"+ Iva)
+else :
+    print("el precio no puede ser negativo")
---
+++ ejer2.py
@@ -0,0 +1,5 @@
+precio= 100
+importe_mensual= precio*0.3
+x=7
+meses= x*importe_mensual
+print("precio aplicando los meses:"+meses)
